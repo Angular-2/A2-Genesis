6cd7b80bf7d55313f06e74319aab8e10e4850527
Nayaata <n.m.keranova@gmail.com>
2016-12-30 02:01:33 +0200

    Create all public components

--- tenn-app-final/src/app/app.component.ts
+++ tenn-app-final/src/app/app.component.ts
@@ -3,6 +3,11 @@ import { NavigationComponent } from './Public/navigation/navigation.component';
 import { RegisterComponent } from './Public/register/register.component';
 import { LoginComponent } from './Public/login/login.component';
 import { HowToUseComponent } from './Public/how-to-use/how-to-use.component';
+import { AboutUsComponent } from './Public/about-us/about-us.component';
+import { ContactsComponent } from './Public/contacts/contacts.component';
+import { ParentGuideComponent } from './Public/parent-guide/parent-guide.component';
+import { TeenJobsComponent } from './Public/teen-jobs/teen-jobs.component';
+
 
 @Component({
   selector: 'app-root',
--- tenn-app-final/src/app/app.module.ts
+++ tenn-app-final/src/app/app.module.ts
@@ -8,13 +8,21 @@ import { AppRoutingModule, routedComponents } from './app-routing.module'
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './Public/navigation/navigation.component';
 import { HowToUseComponent } from './Public/how-to-use/how-to-use.component';
+import { AboutUsComponent } from './Public/about-us/about-us.component';
+import { TeenJobsComponent } from './Public/teen-jobs/teen-jobs.component';
+import { ParentGuideComponent } from './Public/parent-guide/parent-guide.component';
+import { ContactsComponent } from './Public/contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     routedComponents,
-    HowToUseComponent
+    HowToUseComponent,
+    AboutUsComponent,
+    TeenJobsComponent,
+    ParentGuideComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
