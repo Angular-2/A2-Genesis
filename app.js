27783b01b96fdfd99a02ffe05de08bd928bdc54e
Georgi Karaboichev <djorkaevhell@abv.bg>
2017-01-01 21:47:29 +0200

    Implement getUserByUsername

--- app.js
+++ app.js
@@ -39,9 +39,9 @@ app
 	.get('/users', function (req, res, next) {
 		db['users'].find(function (err, users) {
 			if (err) {
-				return res.send(err);
+				return res.json({ "error": "DB Error"});
 			}
-			return res.json(users);
+			return res.status(200).json(users);
 		})
 	})
 	.post('/users', function (req, res, next) {
@@ -58,23 +58,34 @@ app
 			
 				db['users'].save(user, function (err, user) {
 					if (err) {
-						return res.status(400).send(err);
+						return res.status(400).json({"error": "DB error"});
 					}
 					return res.status(200).json(user);
 				})
 			})
 		}
 	})
+	.get('/users/:username', function(req, res, next) {
+		let username = req.params.username;
+
+		db['users'].findOne({ username: username }, function(err, user) {
+			if (!user) {
+				return res.status(401).json({"error": "DB: User not found"});
+			}
+			
+			res.status(200).json(user);
+		});
+	})
 	.post('/authenticate', function(req, res, next)  {
 		if (!req.body.username || !req.body.password) {
-			return res.status(400).send("You must send the username and the password");
+			return res.status(400).json({ "error": "You must send the username and the password" });
 		}
 		db['users'].findOne({ username: req.body.username }, function(err, user) {
 			if (!user) {
-				return res.status(401).send("The username or password doesn't match");
+				return res.status(401).json({ "error": "The username or password doesn't match" });
 			}
 			if (!(user.password === req.body.password)) {
-				return res.status(401).send("The username or password doesn't match");
+				return res.status(401).json({ "error": "The username or password doesn't match" });
 			}
 			
 			res.status(200).send({
@@ -85,7 +96,7 @@ app
 	.get('/jobs', function(req, res, next) {
 		db['jobs'].find({}, function(err, jobs) {
 			if (err) {
-				return res.status(400).send('Error in DB');
+				return res.status(400).josn({ "error": "Error in DB" });
 			}
 
 			return res.status(200).json(jobs);
@@ -96,7 +107,7 @@ app
 
 		db['jobs'].save(job, function(err, user) {
 			if (err) {
-				return res.status(400).send('Error in DB');
+				return res.status(400).json({ "error": "Error in DB" });
 			}
 
 			return res.status(200).json(user);
